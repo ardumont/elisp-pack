73c0d08b0bf36559ef1d5586247a4fdeb51e267a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-30 22:06:27 +0200

    Adding paredit-mode to the emacs-lisp repl ielm.

--- init.el
+++ init.el
@@ -3,5 +3,7 @@
 ;; User this file to initiate the pack configuration.
 ;; See README for more information.
 
+(add-hook 'ielm-mode-hook 'paredit-mode)
+
 ;; Load bindings config
 (live-load-config-file "bindings.el")
