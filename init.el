1bba6bfed833fad7afeb84c55f79a80830b6fe69
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-07-06 20:25:17 +0200

    Transform the ugly ^L into a line, thx again @purcell.

--- init.el
+++ init.el
@@ -2,9 +2,12 @@
                  dash
                  melpa
                  ;; keep the packages locally (offline work)
-                 package-store))
+                 package-store
+                 page-break-lines))
 
 (add-hook 'ielm-mode-hook 'paredit-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'page-break-lines-mode)
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
