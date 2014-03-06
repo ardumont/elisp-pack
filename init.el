bc7db77158d966549a351dd286f37adabad2a58e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-06 06:01:53 +0100

    Remove my not standard indentation rules

--- init.el
+++ init.el
@@ -35,21 +35,7 @@
 ;; activate the reading of line instead of ^L
 (global-page-break-lines-mode t)
 
-;; indentation rule they use bother me
 (require 'dash)
-
-(put 'if 'lisp-indent-function 0)
-(put 'when 'lisp-indent-function 0)
-(put 'unless 'lisp-indent-function 0)
-(put '-> 'lisp-indent-function 0)
-(put '->> 'lisp-indent-function 0)
-(put '--> 'lisp-indent-function 0)
-(put '-when-let 'lisp-indent-function 0)
-(put '-when-let* 'lisp-indent-function 0)
-(put '--when-let 'lisp-indent-function 0)
-(put '-if-let 'lisp-indent-function 0)
-(put '-if-let* 'lisp-indent-function 0)
-(put '--if-let 'lisp-indent-function 0)
 
 (add-to-list 'auto-mode-alist '("Cask$" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.feature$" . perl-mode))
