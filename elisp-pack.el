8d37e19b201bccc09c4882103c8fc5b8bdbb853f
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-11 10:19:09 +0100

    Format requirements

--- elisp-pack.el
+++ elisp-pack.el
@@ -4,22 +4,26 @@
 
 ;;; Code:
 
+;; internal libs
+(require 'lisp-mode)
+(require 'cl-lib)
+(require 'autoinsert)
+(require 'ielm)
+(require 'files)
+
+;; external libs
 (require 'bug-hunter)
 (require 'el-mock)
 (require 'goto-last-change)
-(require 'cl-lib)
-(require 'dash)
 (require 'dash-functional)
+(require 'aggressive-indent)
+(require 'overseer)
+(require 'page-break-lines)
+(require 'emr)
 
-(require 'autoinsert)
 (auto-insert-mode 1)
 
-(require 'aggressive-indent)
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
-
-(require 'overseer)
-
-(require 'lisp-mode)
 (add-hook 'emacs-lisp-mode-hook
 	  (lambda ()
 	    (local-set-key (kbd "C-c m b") 'eval-buffer)
@@ -28,16 +32,11 @@
 	    (local-set-key (kbd "C-c m d") 'eval-defun)
 	    (local-set-key (kbd "C-c m n") 'eval-print-last-sexp)))
 (add-hook 'emacs-lisp-mode-hook 'overseer-mode)
-
-(require 'ielm)
 (add-hook 'ielm-mode-hook 'paredit-mode)
 
-;; to display a beautiful line instead of the ugly ^L
-(require 'page-break-lines)
-;; activate the reading of line instead of ^L
+;; to display beautiful lines instead of ugly ^L
 (global-page-break-lines-mode t)
 
-(require 'files)
 (add-to-list 'auto-mode-alist '("Cask$" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.feature$" . perl-mode))
 
@@ -53,7 +52,6 @@ This command assumes point is not in a string or comment."
       (forward-sexp (or arg 1))
       (copy-region-as-kill opoint (point)))))
 
-(require 'emr)
 (autoload 'emr-show-refactor-menu "emr")
 (add-hook 'emacs-lisp-mode-hook 'emr-initialize)
 
